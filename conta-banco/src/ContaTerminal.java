1dea45f4f9971b4cbde1b1d738e9c37489439206
gabscodin <gabyh.332@gmail.com>
2023-10-17 04:58:19 -0300

    atualizações do projeto

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+    Scanner scanner = new Scanner (System.in);
+
+    System.out.println("Digite o número da sua agência: ");
+    String agencia = scanner.next();
+
+    System.out.println("Digite o número da sua conta: ");
+    int conta = scanner.nextInt();
+
+    System.out.println("Digite o seu nome completo: ");
+    String nomeCliente = scanner.next();
+
+    System.out.println("Insira o seu saldo da conta: ");
+    scanner.nextLine();
+    double saldo = scanner.nextDouble();
+
+    scanner.close();
+
+    System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +", conta " + conta +", e seu saldo R$ " + saldo + " já está disponível para saque.");
+
+
+
     }
 }
