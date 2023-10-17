cf9ef62f560ad5ea9ae848f98d415083f44a5bda
gabscodin <gabyh.332@gmail.com>
2023-10-17 05:18:30 -0300

    entregue

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -2,27 +2,28 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    Scanner scanner = new Scanner (System.in);
+        System.out.println("Digite o número da sua agência: ");
+        String agencia = scanner.next();
+        System.out.println();
 
-    System.out.println("Digite o número da sua agência: ");
-    String agencia = scanner.next();
+        System.out.println("Digite o número da sua conta: ");
+        int conta = scanner.nextInt();
+        System.out.println();
 
-    System.out.println("Digite o número da sua conta: ");
-    int conta = scanner.nextInt();
+        System.out.println("Digite o seu nome completo (nome e sobrenome): ");
+        scanner.nextLine(); 
+        String nomeCompleto = scanner.nextLine();
 
-    System.out.println("Digite o seu nome completo: ");
-    String nomeCliente = scanner.next();
+        System.out.println();
+        System.out.println("Insira o seu saldo da conta: ");
+        double saldo = scanner.nextDouble();
 
-    System.out.println("Insira o seu saldo da conta: ");
-    scanner.nextLine();
-    double saldo = scanner.nextDouble();
+        scanner.close();
 
-    scanner.close();
-
-    System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +", conta " + conta +", e seu saldo R$ " + saldo + " já está disponível para saque.");
-
-
-
+        System.out.println();
+        System.out.println("Olá " + nomeCompleto + ", obrigado por criar uma conta em nosso banco.");
+        System.out.println("Sua agência é " + agencia + ", conta " + conta + ", e seu saldo R$ " + saldo + " já está disponível para saque.");
     }
 }
